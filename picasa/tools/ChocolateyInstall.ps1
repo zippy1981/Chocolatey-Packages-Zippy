bef784eab989170a80530ca90215a2a78778c99f
Justin Dearing <zippy1981@gmail.com>
2013-06-27 09:38:35 -0400

    apparently Alan Stavens beat me to this.

--- picasa/tools/ChocolateyInstall.ps1
+++
@@ -1,8 +0,0 @@
-$id = 'picasa';
-$url = 'https://dl.google.com/picasa/picasa39-setup.exe';
-try {
-  Install-ChocolateyPackage $id 'exe' '/S /L' $url
-} catch {
-  Write-ChocolateyFailure $id "$($_.Exception.Message)"
-  throw 
-}
