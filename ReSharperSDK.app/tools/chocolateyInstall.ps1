8373cdbab0e13ad6d7597159057c94bf2d8cd918
Justin Dearing <justin.dearing@rms.com>
2013-08-12 08:49:00 -0400

    bumped Resharper SDK up to version 8 and added version 7 meta packages

--- ReSharperSDK.app/tools/chocolateyInstall.ps1
+++ ReSharperSDK.app/tools/chocolateyInstall.ps1
@@ -1,2 +1,2 @@
 Install-ChocolateyPackage 'ReSharper.SDK.msi' 'msi' '/quiet' `
-	'http://download.jetbrains.com/resharper/ReSharperSDK-7.1.96.msi'
+	'http://download.jetbrains.com/resharper/ReSharperSDK-8.0.1086.msi'
--- ReSharperSDK.tool/tools/chocolateyInstall.ps1
+++ ReSharperSDK.tool/tools/chocolateyInstall.ps1
@@ -1,3 +1,3 @@
 Install-ChocolateyZipPackage 'ReSharper.SDK.tool' `
-	'http://download.jetbrains.com/resharper/ReSharperSDK-7.1.96.zip' `
+	'http://download.jetbrains.com/resharper/ReSharperSDK-8.0.1086.zip' `
 	"$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
