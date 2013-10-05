addd417174a48cb741153c9c426c6b2bb490b002
Justin Dearing <zippy1981@gmail.com>
2013-10-05 09:39:44 -0400

    Bumped AnkhSvn to 2.5.12116

--- AnkhSvn/tools/ChocolateyInstall.ps1
+++ AnkhSvn/tools/ChocolateyInstall.ps1
@@ -1,4 +1,4 @@
 Install-ChocolateyPackage `
 	'AnkhSvn' 'msi' '/passive' `
-	'http://ankhsvn.open.collab.net/files/documents/175/7480/AnkhSvn-2.5.12040.msi'
+	'http://ankhsvn.open.collab.net/files/documents/175/7695/AnkhSvn-2.5.12116.msi'
 	
--- AnkhSvn/tools/ChocolateyUninstall.ps1
+++ AnkhSvn/tools/ChocolateyUninstall.ps1
@@ -2,7 +2,7 @@ $package = 'AnkhSvn'
 
 try {
   # HKEY_LOCAL_MACHINE\SOFTWARE\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall\
-  $msiid = '{93092EE7-0D85-40AD-B9AF-3BAC2787E267}'
+  $msiid = '{8DE45E4D-7589-4D0B-B083-7812357BA235}'
   Uninstall-ChocolateyPackage $package 'MSI' -SilentArgs "$msIid /qb" -validExitCodes @(0)
   
   # the following is all part of error handling
