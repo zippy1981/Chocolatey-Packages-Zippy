b1a254791bf02bc7aa841eaf10adbbb202cde5b9
Justin Dearing <zippy1981@gmail.com>
2013-12-20 22:28:08 -0500

    bumped dottrace to 5.5.3.554.

--- dotTrace/tools/chocolateyInstall.ps1
+++ dotTrace/tools/chocolateyInstall.ps1
@@ -1,6 +1,6 @@
 try {
 
-  Install-ChocolateyPackage 'dotTrace' 'msi' '/passive /qn' 'http://download-ln.jetbrains.com/dottrace/dotTraceSetup.5.5.0.24440.msi'
+  Install-ChocolateyPackage 'dotTrace' 'msi' '/passive /qn' 'http://download-ln.jetbrains.com/dottrace/dotTraceSetup.5.5.3.554.msi'
 
 } catch {
 
--- dotTrace/tools/chocolateyUninstall.ps1
+++ dotTrace/tools/chocolateyUninstall.ps1
@@ -2,7 +2,7 @@ $package = 'dotTrace'
 
 try {
   # HKEY_LOCAL_MACHINE\SOFTWARE\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall\
-  $msiid = '{6F9F4527-229C-4A6B-98A4-6874DCF5309F}'
+  $msiid = '{DBC020DC-C118-4E18-A5A7-40234850CCBA}'
   Uninstall-ChocolateyPackage $package 'MSI' -SilentArgs "$msIid /qb" -validExitCodes @(0,1603)
 
   # the following is all part of error handling
