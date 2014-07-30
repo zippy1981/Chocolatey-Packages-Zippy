467417aba34be7650783d30e5b640098546cdeee
Justin Dearing <zippy1981@gmail.com>
2014-07-30 13:01:14 -0400

    Updated Ditto to 3.19.24.0

--- ditto/tools/chocolateyInstall.ps1
+++ ditto/tools/chocolateyInstall.ps1
@@ -1,8 +1,8 @@
 ﻿# The installer doesn't properly kill ditto
 Get-Process | Where { $_.name -eq 'ditto' } | Stop-Process
 Install-ChocolateyPackage 'ditto' 'exe' '/sp- /silent /norestart' `
-	'https://sourceforge.net/projects/ditto-cp/files/Ditto/3.18.46.0/DittoSetup_3_18_46_0.exe/download' `
-	'https://sourceforge.net/projects/ditto-cp/files/Ditto/3.18.46.0/DittoSetup_64bit_3_18_46_0.exe/download'
+	'http://iweb.dl.sourceforge.net/project/ditto-cp/Ditto/3.19.24.0/DittoSetup_3_19_24_0.exe' `
+	'http://superb-dca3.dl.sourceforge.net/project/ditto-cp/Ditto/3.19.24.0/DittoSetup_64bit_3_19_24_0.exe'
 Set-ItemProperty -Path HKCU:\Software\Ditto\CopyStrings -name 'powershell.exe' -value '% {{Delay100}ey'
 Set-ItemProperty -Path HKCU:\Software\Ditto\PasteStrings -name 'powershell.exe' -value '% {{Delay100}ep'
 # It also doesn't start the process
