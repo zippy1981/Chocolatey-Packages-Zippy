0a698f7aca0eb99b75a39bc8d433b86c6cf87763
Justin Dearing <zippy1981@gmail.com>
2014-11-21 10:04:56 -0500

    Bumped AnkdSvn to 2.5.12478

--- AnkhSvn/tools/ChocolateyInstall.ps1
+++ AnkhSvn/tools/ChocolateyInstall.ps1
@@ -1,4 +1,3 @@
 ﻿Install-ChocolateyPackage `
 	'AnkhSvn' 'msi' '/passive' `
-	'https://ankhsvn.open.collab.net/files/documents/175/9134/AnkhSvn-2.5.12471.17.msi'
-	
+	'https://ankhsvn.open.collab.net/files/documents/175/9669/AnkhSvn-2.5.12478.msi'	
--- AnkhSvn/tools/ChocolateyUninstall.ps1
+++ AnkhSvn/tools/ChocolateyUninstall.ps1
@@ -2,7 +2,7 @@
 
 try {
   # HKEY_LOCAL_MACHINE\SOFTWARE\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall\
-  $msiid = '{0FF62119-FA0A-4C09-99D5-B5AEB0AF9556}'
+  $msiid = '{A6E0F8A8-469D-4109-BC4B-F67564065069}'
   Uninstall-ChocolateyPackage $package 'MSI' -SilentArgs "$msIid /qb" -validExitCodes @(0)
   
   # the following is all part of error handling
