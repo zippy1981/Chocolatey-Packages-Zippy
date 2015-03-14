1cfe5461804c7f7e1ce3f33a208a1d4a9265e441
Justin Dearing <zippy1981@gmail.com>
2015-03-14 00:40:11 -0400

    bumped pycharms to 4.0.5

--- PyCharm-community/tools/chocolateyInstall.ps1
+++ PyCharm-community/tools/chocolateyInstall.ps1
@@ -1,6 +1,6 @@
 ﻿$packageName = 'PyCharm' # arbitrary name for the package, used in messages
 $installerType = 'EXE' #only one of these two: exe or msi
-$url = 'http://download-cf.jetbrains.com/python/pycharm-community-4.0.4.exe' # download url
+$url = 'http://download-cf.jetbrains.com/python/pycharm-community-4.0.5.exe' # download url
 $url64 = $url # 64bit URL here or just use the same as $url
 $silentArgs = '/S' # "/s /S /q /Q /quiet /silent /SILENT /VERYSILENT" # try any of these to get the silent installer #msi is always /quiet
 $validExitCodes = @(0) #please insert other valid exit codes here, exit codes for ms http://msdn.microsoft.com/en-us/library/aa368542(VS.85).aspx
